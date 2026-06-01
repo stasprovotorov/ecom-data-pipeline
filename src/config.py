5e1e40f4fd437d2e8f81b3253cb2ffb108eca5b6
Stanislav Provotorov <stas_provotorov@Stanislavs-MacBook-Pro.local>
2026-06-01 17:35:38 +0400

    replace pydantic-settings with dotenv

--- src/config.py
+++ src/config.py
@@ -1,21 +1,23 @@
-from pathlib import Path
-from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from dataclasses import dataclass
+from dotenv import load_dotenv
 
-ROOT = Path(__file__).resolve().parents[1]
+load_dotenv()
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(
-        env_file=f'{ROOT}/.env',
-        env_file_encoding='utf-8',
-    )
-
+@dataclass
+class Settings:
     DB_URL: str
-
     CSV_USERS: str
+    CSV_PRODUCTS: str
     CSV_ORDERS: str
     CSV_ORDER_ITEMS: str
-    CSV_PRODUCTS: str
 
 
-settings = Settings()
+settings = Settings(
+    DB_URL=os.getenv('DB_URL'),
+    CSV_USERS=os.getenv('CSV_USERS'),
+    CSV_PRODUCTS=os.getenv('CSV_PRODUCTS'),
+    CSV_ORDERS=os.getenv('CSV_ORDERS'),
+    CSV_ORDER_ITEMS=os.getenv('CSV_ORDER_ITEMS'),
+)
